e6088f6d5ecadb56dbf5dcbda48551a77b726a34
Riyaz Saiyed <riyaz.saiyed@emirates.com>
2019-11-24 11:56:27 +0400

    unit

--- app/src/components/customer/Table.test.tsx
+++ app/src/components/customer/Table.test.tsx
@@ -2,8 +2,21 @@ import * as React from 'react';
 import {shallow} from "enzyme";
 import Table from "./Table";
 
+const customersData = [{
+    id: 1,
+    name: "Alice",
+    cardNumber: "1111 2222 3333 4444",
+    balance: -1045,
+    limit: "2000",
+    currency: "$"
+}];
 
 it('renders the empty table', () => {
     const wrapper = shallow(<Table customers={[]}/>);
     expect(wrapper.find('h2').text()).toBe('Existing Cards');
+});
+
+it('renders the table with data', () => {
+    const wrapper = shallow(<Table customers={customersData}/>);
+    expect(wrapper.find('#tableDataDiv').html()).toContain('<td>Alice</td>');
 });
